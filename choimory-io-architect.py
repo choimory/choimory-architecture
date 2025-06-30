1dbce7d71ff925762a6a2b23e9e0a7aea675cc16
choimory <choimory@naver.com>
2025-06-30 22:14:32 +0900

    fix: cqrs로 변경

--- choimory-io-architect.py
+++ choimory-io-architect.py
@@ -10,6 +10,7 @@ from diagrams.onprem.ci import GithubActions
 from diagrams.k8s.compute import RS, Pod, Deploy
 from diagrams.aws.compute import EC2
 from diagrams.aws.storage import S3
+from diagrams.elastic.elasticsearch import Elasticsearch
 
 
 with Diagram("choimory-io", direction="BT"):
@@ -19,33 +20,38 @@ with Diagram("choimory-io", direction="BT"):
         with Cluster("Web"):
             front = Nextjs("client")
             user >> front
-            
-        with Cluster("API Gateway"):
-            gateway = Java("api-gateway")
-            front >> gateway
-
-        with Cluster("Message broker"):
-            broker = Rabbitmq("broker")
-            gateway >> broker
 
         with Cluster("API"):
             member_api = Kotlin("member-api")
             memo_api = Nodejs("memo-api")
-            broker >> member_api
-            broker >> memo_api
-        
+            front >> member_api
+            front >> memo_api
+
         with Cluster("Database"):
-            member_db = Postgresql("member-db")
-            memo_db = MongoDB("memo-db")
-            member_api >> member_db
-            memo_api >> memo_db
+            member_command = Postgresql("member-command")
+            member_query = Elasticsearch("member-query")
+            
+            memo_command = Postgresql("memo-command")
+            memo_query = MongoDB("memo-query")        
+
+            member_api >> member_command
+            member_api >> member_query
+            memo_api >> memo_command
+            memo_api >> memo_query
+
+        with Cluster("Broker"):
+            broker = Rabbitmq("broker")
+
+            member_command >> broker
+            member_query >> broker
+            memo_command >> broker
+            memo_query >> broker 
 
     with Cluster("CI"):
         github = Github("github")
-        front >> github
-        gateway >> github
-        member_api >> github
-        memo_api >> github  
+        #front >> github
+        #member_api >> github
+        #memo_api >> github  
 
         github_actions = GithubActions("github-actions")
         github >> github_actions
